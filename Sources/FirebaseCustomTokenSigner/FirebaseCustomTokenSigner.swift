366c6d24037f80fb64fe24c38a9bb6f029197e3f
kevinzhow <kevinchou.c@gmail.com>
2023-12-22 20:37:56 +0900

    Update signer access control

--- Sources/FirebaseCustomTokenSigner/FirebaseCustomTokenSigner.swift
+++ Sources/FirebaseCustomTokenSigner/FirebaseCustomTokenSigner.swift
@@ -1,10 +1,10 @@
 import JWTKit
 
-struct FirebaseCustomTokenSigner {
+public struct FirebaseCustomTokenSigner {
     let serviceAccountEmail: String
     let privateKey: String
 
-    func sign(uid: String) throws -> String {
+    public func sign(uid: String) throws -> String {
         let signer = JWTSigner.rs256(key: try .private(pem: privateKey.data(using: .utf8)!))
         let payload = FireabseUserPayload(
             issuer: IssuerClaim(value: serviceAccountEmail),
